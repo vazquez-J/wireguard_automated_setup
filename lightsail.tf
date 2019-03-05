4dc8224525617c44fdb085ce5263bd26a1a86ef0
vazquez-J <vazquez.jv10@protonmail.com>
2019-03-04 17:15:05 -0800

    Lightsail terraform file added without key_pair_name

---
+++ lightsail.tf
@@ -0,0 +1,8 @@
+resource "aws_lightsail_instance" "wireguard_client" {
+  name              = "wireguard"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "debian_8_7"
+  bundle_id         = "nano_us-east2:2_0"
+  key_pair_name     = ""
+
+}
